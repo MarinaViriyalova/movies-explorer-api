d5d7ec034f30b86c33f1f55965106fb49957e1fa
MarinaViriyalova <m.viriyalova@gmail.com>
2022-02-21 18:55:20 +0300

    cors?

---
+++ middlewares/cors.js
@@ -0,0 +1,27 @@
+const allowedCors = [
+    'http://localhost:3000',
+    'https://viriyalova-diploma.nomoredomains.work',
+    'http://viriyalova-diploma.nomoredomains.work'
+];
+
+module.exports = (req, res, next) => {
+    const { origin } = req.headers;
+    const { method } = req;
+    const requestHeaders = req.headers['access-control-request-headers'];
+
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+    console.log('111', origin)
+
+    if (allowedCors.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Credentials', true);
+    }
+    if (method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+        res.header('Access-Control-Allow-Headers', requestHeaders);
+        return res.end();
+    }
+
+    next();
+};
